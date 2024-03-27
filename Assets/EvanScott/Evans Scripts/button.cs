f6d61a0f25b8b8bf6ac902ecca3205eb6e062153
Tonson5 <62815448+Tonson5@users.noreply.github.com>
2024-03-27 09:15:40 -0700

    interactions

--- Assets/EvanScott/Evans Scripts/button.cs
+++ Assets/EvanScott/Evans Scripts/button.cs
@@ -8,6 +8,14 @@ public class button : MonoBehaviour
     public bool interacted;
     public bool doorButton = true;
     public GameObject player;
+    public bool toothbrush = false;
+    public bool bed = false;
+    public bool breakerBox = false;
+    public GameObject gameManager;
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
     public void Interacted()
     {
         if (doorButton)
@@ -18,12 +26,29 @@ public class button : MonoBehaviour
                 door.GetComponent<doorIdentifier>().opened = true;
                 interacted = true;
             }
-            else if(door.GetComponent<doorIdentifier>().opened == true && interacted == false)
+            else if (door.GetComponent<doorIdentifier>().opened == true && interacted == false)
             {
                 door.GetComponent<doorIdentifier>().opened = false;
                 interacted = true;
             }
-            
+
         }
+
+        if (toothbrush)
+        {
+            gameManager.GetComponent<gameManager>().toothbrushUsed = true;
+            interacted = true;
+        }
+        else if (bed)
+        {
+            gameManager.GetComponent<gameManager>().bedMade = true;
+            interacted = true;
+        }
+        else if (breakerBox)
+        {
+            gameManager.GetComponent<gameManager>().lightsOn = true;
+            interacted = true;
+        }
+
     }
 }
